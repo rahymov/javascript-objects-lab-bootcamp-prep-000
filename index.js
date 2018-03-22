c8505815334112eb6c8a8d443bd11ebafa6d4128
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-22 17:43:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  delete newObj.key;
+  delete newObj[key];
   return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
